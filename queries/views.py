3f31d600ba5960122374c64686ec9fab134b9933
EricWithC04 <negativegame117@gmail.com>
2024-01-11 16:34:59 -0300

    feat: include

--- queries/views.py
+++ queries/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("<h1>Hello, world. You're at the polls index.</h1>")
+
+def about(request):
+    return HttpResponse("<h1>About</h1>")
+
+def main(request):
+    return HttpResponse("<h1>Main</h1>")
