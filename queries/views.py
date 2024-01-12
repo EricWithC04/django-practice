62aaac62bdcfa358d9ae51e6963afb699cd8ca7c
EricWithC04 <negativegame117@gmail.com>
2024-01-12 15:35:39 -0300

    method render

--- queries/views.py
+++ queries/views.py
@@ -1,27 +1,27 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from .models import Project, Task
 
 # Create your views here.
 def hello(request):
-    return HttpResponse("<h1>Hello, world. You're at the polls index.</h1>")
+    return render(request, "hello.html")
 
 def about(request):
-    return HttpResponse("<h1>About</h1>")
+    return render(request, "about.html")
 
 def main(request):
-    return HttpResponse("<h1>Main</h1>")
+    return render(request, "main.html")
 
 def user(request, user_id):
     return HttpResponse(f"<h1>Hello {user_id}!</h1>")
 
 def get_all_projects(request):
-    all_projects = list(Project.objects.values())
-    return JsonResponse(all_projects, safe=False)
+    # all_projects = list(Project.objects.values())
+    return render(request, "projects.html")
 
 def get_all_tasks(request):
-    all_tasks = list(Task.objects.values())
-    return JsonResponse(all_tasks, safe=False)
+    # all_tasks = list(Task.objects.values())
+    return render(request, "tasks.html")
 
 def task_by_id(request, id):
     task = get_object_or_404(Task, id=id)
